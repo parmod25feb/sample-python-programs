27c60084f656c86222ec7a0d5223d6e52dab3394
Parmod Kumar <parmodku@google.com>
2019-12-27 14:53:05 -0800

    Added comments to the script

--- wordcountinstring.py
+++ wordcountinstring.py
@@ -1,21 +1,21 @@
 # Lint as: python3
-"""This program will show how to count words in the string"""
+"""This program will show words and their occurrences when >2"""
 
 
 original = "Hello there, hello buddy Hello Python python is cool"
 print("Your stirng is : ", original.lower())
 dic = {}
-original = original.lower()
-lst = original.split(' ')
+original = original.lower()   # Converting string to lowercase
+lst = original.split(' ')     # Splitting the string with spaces and adding into the list
 
-for item in lst:
+for item in lst:              # Iterating through the list and checking the words
   if item in dic.keys():
     dic[item]=dic[item] + 1
   else: 
     dic[item] = 1
 
 
-for value in dic.keys():
+for value in dic.keys():     
   if dic[value] > 1:
     print("Word - ",value, " is occurring : ", dic[value]," -times")
 
