9baead5e641052c039faaaee41a6a7d0b95dcc24
Parmod Kumar <parmodku@google.com>
2020-01-02 14:35:20 -0800

    added comments to the script

--- armstrongnumbers.py
+++ armstrongnumbers.py
@@ -2,15 +2,20 @@
 
 print("Here is the list of all the Armstrong numbers less then < 1000")
 
+#Iterating through the numbers from 0 to 1000
 for num in range(0,1000):
-  rem =0
+  rem =0           #declaring local variables
   rev =0 
   num1 =0
   num1=num
-  while num1 != 0:
+  while num1 != 0:               # logic for Armstrong number
     rem = num1 % 10
     rev = rev + rem**3
     num1 = num1//10
 
-  if num == rev:
+  if num == rev:                 # checking and pritning if the number is armstrong number
     print(" ",rev)
+
+
+
+# Output is  : 0, 1, 153, 370, 371, 407
