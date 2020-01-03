a2aec3310f031779ed221b63460d461205ac2170
Parmod Kumar <parmodku@google.com>
2020-01-03 14:50:30 -0800

    added comments to factorial script

--- factorial.py
+++ factorial.py
@@ -1,12 +1,14 @@
 # This program is to calculate the Factorial of a number
 
+# Asking user to enter the number and converting that to int 
 num = int(input("Please enter your number : "))
 
+# Function to calculate Factorial
 def factorial(num):
   if num ==0 or num==1:
     return 1
   else:
     return num*factorial(num-1)
 
-fact=factorial(num)
-print("Factorial of %d is %d"%(num,fact))
+fact=factorial(num)  # Calling the factorial function by passing the num argument
+print("Factorial of %d is %d"%(num,fact)) # Printing the number and factorial value
