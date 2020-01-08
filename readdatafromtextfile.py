eef7ab82ca3927be8fe11e35faa6dace8cd6de00
Parmod Kumar <parmodku@google.com>
2020-01-07 16:14:58 -0800

    added comments to the file

--- readdatafromtextfile.py
+++ readdatafromtextfile.py
@@ -1,7 +1,10 @@
 # In this script will see how to read data from a text file
 
+# First will open the text file by providing the path and will open in read mode
 FILE = open("/usr/local/google/home/parmodku/Downloads/pythonprograms/datafile.txt","r")
 
+# Read the content of the file
 content = FILE.read()
 
+# Print the content of the file
 print("File content is : \n\n",content)
