8a687de6154bab38a0631b43485dcf3be52fb4c4
Parmod Kumar <parmodku@google.com>
2020-01-10 17:02:31 -0800

    Added comment to the file

--- coolstring.py
+++ coolstring.py
@@ -1,22 +1,22 @@
 # This is a python program to print a string "cool" equal "co2l" means if the count is > 1 then only it should print the numeric count
 
-st = input("Please enter the string : ")
-dic = {}
+st = input("Please enter the string : ") # Ask user to enter the string
+dic = {}                                 # Declare and empty dictionary to store data in key value pair
 
-for ch in st:
-  if ch in dic.keys():
-    dic[ch]=dic[ch]+1
+for ch in st:                            # Start iteration through string character by character
+  if ch in dic.keys():                   # Check if the key is in Dictionary 
+    dic[ch]=dic[ch]+1                    # If yes, then will increase the value by one and will add in dictionary
   else:
-    dic[ch]=1
+    dic[ch]=1                            # Else will store 1 for the key value 
 
 print("Here is the new string : ")
-for key in dic.keys():
-  if dic[key] > 1:
-    print("%c%d"%(key,dic[key]),end="")
+for key in dic.keys():                   # Now will iterate through the dictionary
+  if dic[key] > 1:                       # Will check if key value is >1 then only will print numeric value else will print key only
+    print("%c%d"%(key,dic[key]),end="")  # end="" will keep printing the value in the same line
   else:
     print(key,end="")
 
-print("\n\n")
+print("\n\n")                            # This is just for newline
 
 
 #Output: if entered cool then it would be co2l,if entered abbcccdde then it will be ab2c3d2e
