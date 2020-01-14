151b68bd5df3f56be3f9b414db201aea48c28666
Parmod Kumar <parmodku@google.com>
2020-01-13 16:09:57 -0800

    added valuable comments to the file

--- stringreverse.py
+++ stringreverse.py
@@ -4,11 +4,11 @@ st = input("Pls enter your string : ")
 print("Original string is : %s"%st)
 
 # String reverse using string slice method
-def streverse(st):
+def streverse(st):              # Defined the function to do the string reverse
   count = len(st)
   return st[count::-1]
 
-st_reverse1 = streverse(st)
+st_reverse1 = streverse(st)    # Calling the function to reverse the string
 print("String reverse using slice method is : %s"%st_reverse1)
 
 
@@ -16,15 +16,15 @@ print("String reverse using slice method is : %s"%st_reverse1)
 print("String reverse using range function is : ",end='')
 count = len(st)
 lst = []
-for ch in range(count-1,-1,-1):
+for ch in range(count-1,-1,-1): # Iterating through the string length in a reverse way
   print(st[ch],end='')
 
 
 # String reverse using another string
 
-st_rev=""
+st_rev=""        # Empty string
 for ch in st:
-  st_rev= ch+st_rev
+  st_rev= ch + st_rev  
 
 print("\nString reverse using another empty string is  : %s"%st_rev)
 
